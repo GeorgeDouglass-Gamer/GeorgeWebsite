0a964654db5759fe86ef95845c73a8a3e7886439
GeorgeDouglass-Gamer <gdouglass@tfssa.org>
2021-03-08 14:33:37 -0600

    made the program finally work

--- GolfWebsite/ScraperTest.py
+++ GolfWebsite/ScraperTest.py
@@ -2,23 +2,43 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-listD=[]
-res = requests.get(
-    'https://www.golfdigest.com/hot-list/golf-clubs/drivers')
+classtopull=".m-Card__m-Content__a-Title"
+websiteURL='https://www.golfdigest.com/hot-list/golf-clubs/drivers'
+jsonfile="DriverNames.json"
+duplicate=[]
+webList=[]
+NewList=[]
+res = requests.get(websiteURL)
 txt = res.text
 status = res.status_code
 
 soup=BeautifulSoup(res.content, 'html.parser')
 page_title = soup.title.text
 
-for i in range(len(soup.select(".m-Card__m-Content__a-Title"))):
-    txt=(soup.select('.m-Card__m-Content__a-Title')[i].text)
+#scrapes website for data and puts it into list
+for i in range(len(soup.select(classtopull))):
+    txt=(soup.select(classtopull)[i].text)
     word=txt.strip()
-    listD.append(word)
-# for i in listD:
-#     print (i)
-#
-# print (listD)
+    webList.append(word)
+webList.pop()
 
-with open("Data.json", 'w') as f:
-    json.dump(listD,f)
+#loads old json file and puts it into list FileData
+with open(jsonfile, 'r') as f:
+    FileData=json.load(f)
+
+#adds old contents of the file to the new list
+for j in FileData:
+    NewList.append(j)
+
+#make sure it doesn't put the same driver in twice and creates new list.
+for j in webList:
+    shouldIAdd=True
+    for i in FileData:
+        if j == i:
+            shouldIAdd=False
+    if shouldIAdd == True:
+        NewList.append(j)
+
+#puts new list into the json file
+with open(jsonfile, 'w') as f:
+    json.dump(NewList,f)
